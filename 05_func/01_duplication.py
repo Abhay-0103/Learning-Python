448f3a1f967ba4c961fe8cf44b237d9a3d5b4500
Abhay-0103 <ab0321054@gmail.com>
2026-02-15 00:46:01 +0530

    Add function to print chai orders

---
+++ 05_func/01_duplication.py
@@ -0,0 +1,8 @@
+def print_order(name, chai_type):
+    print(f"{name} ordered a {chai_type} chai.")
+
+
+
+print_order("Abhay", "Maslasa")
+print_order("Satyarth", "Ginger")
+print_order("Yeno", "Ginger")
