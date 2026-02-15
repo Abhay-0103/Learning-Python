123e58678c98666e66e03a84665a827095e08ac4
Abhay-0103 <ab0321054@gmail.com>
2026-02-16 00:18:25 +0530

    Add user registration functions for input, validation, and database saving

---
+++ 05_func/03_hiding.py
@@ -0,0 +1,20 @@
+def get_intput():
+    print("Getting User Input...")
+
+
+def validate_input():
+    print("Validating User Input...")
+
+
+def save_to_database():
+    print("Saving to Database...")  
+
+
+def register_User():
+    get_intput()
+    validate_input()
+    save_to_database()
+    print("User Registered Successfully!")
+
+
+register_User()
