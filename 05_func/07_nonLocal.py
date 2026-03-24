2562e876f2fce50e9f3eb3a402fa3eaa2de89847
Abhay-0103 <ab0321054@gmail.com>
2026-03-25 00:36:22 +0530

    Local Scop

---
+++ 05_func/07_nonLocal.py
@@ -0,0 +1,10 @@
+def update_order():
+    chai_type = "Elaichii"
+
+    def kitchen():
+        nonlocal chai_type
+        chai_type = "Masala Chai"
+    kitchen()
+    print("After kitchen update", chai_type)
+
+update_order()
