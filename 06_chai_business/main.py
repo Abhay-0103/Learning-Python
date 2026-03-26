104284fa5bf5222b3ae23faf147b28b7c7d828f9
Abhay-0103 <ab0321054@gmail.com>
2026-03-26 21:36:04 +0530

    importing and exporting

---
+++ 06_chai_business/main.py
@@ -0,0 +1,15 @@
+# import recipes.flavors
+
+# print(recipes.flavors.hello_gulab_jamun())
+
+
+# from recipes.flavors import hello_gulab_jamun , hello_baby
+
+# print(hello_gulab_jamun())
+
+
+# --- old method not recommended ---
+# from .recipes.flavors import hello_gulab_jamun , hello_baby
+
+# print(hello_gulab_jamun())
+# print(hello_baby())
---
+++ 06_chai_business/recipes/flavors.py
@@ -0,0 +1,5 @@
+def hello_baby():
+    return "Hello, Baeee ! Are you still alive ?"
+
+def hello_gulab_jamun():
+    return "Hello, My Gulab Jamun ! let's have some fun togight ?"
