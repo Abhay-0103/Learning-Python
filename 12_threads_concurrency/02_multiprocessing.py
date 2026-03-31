e8af337511bf14d345f3fa552884d9a46a57686a
Abhay-0103 <ab0321054@gmail.com>
2026-03-31 22:55:53 +0530

    processing

---
+++ 12_threads_concurrency/02_multiprocessing.py
@@ -0,0 +1,26 @@
+from multiprocessing import Process
+import time
+
+
+def brew_chai(name):
+    print(f"Brewing chai for {name}")
+    time.sleep(3)
+    print(f"End of {name} chai brewing")
+
+
+if __name__ == "__main__":
+    chai_makers = [
+        Process(target=brew_chai, args=(f"Chai Maker #{i+1}", ))
+        for i in range(3)
+    ]
+
+    # Start all processes
+    for p in chai_makers:
+        p.start()
+
+    # Wait for all processes to finish
+    for p in chai_makers:
+        p.join()
+
+
+    print("All chai brewed")
