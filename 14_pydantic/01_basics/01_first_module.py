7cf08f2d36d4dcf036c52291843a80336d4a3c09
Abhay-0103 <ab0321054@gmail.com>
2026-04-04 22:43:12 +0530

    intro

---
+++ 14_pydantic/01_basics/01_first_module.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: int
+    name: str
+    is_active: bool
+
+
+input_data = {
+    'id': 101,
+    'name': "Lucky",
+    'is_active': True
+}
+
+user = User(**input_data)
+print(user)
